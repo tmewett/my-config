d7ea993ff5361ee77259401deebae07e7a1a79e3
Tom M <tom@collider.in>
2022-07-04 16:05:15 +0100

    Sync

--- .config/fish/functions/man.fish
+++ .config/fish/functions/man.fish
@@ -3,7 +3,7 @@ function man --wraps=man
         # We have to use a new temp file for openers which would reload on change.
         set file (mktemp)
         command man $argv > $file
-        $NUI_MAN_OPENER $file
+        and $NUI_MAN_OPENER $file
         # We can't delete since the opener may take some time to read the file.
     else
         command man $argv
