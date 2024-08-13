83881326afa4c6e6648f4ee5ff895b64a5794e35
Tom Mewett <tom.mewett@frvr.com>
2024-08-13 17:13:02 +0100

    Sync

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,18 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    abbr --add xc 'x code .'
+    abbr --add xcg 'x code .; t lazygit'
+    
+    function fish_user_key_bindings
+        fzf_key_bindings
+    end
+    source /opt/local/share/fzf/shell/key-bindings.fish
 end
 
-abbr --add xc 'x code .'
-abbr --add xcg 'x code .; t lazygit'
+# -j .5 - search results appear in middle of screen
+# -R - don't escape colours and text effects
+# -P prompt - the default medium/-m prompt, modified to always show the file
+#   (not just on first prompt) and appended with help info like 'man'
+set -gx LESS "-j .5 -R -P ?f%f :- .?m(%T %i of %m) .?e(END) ?x- Next\: %x.:?pB%pB\%:byte %bB?s/%s...%t (press h for help or q to quit)\$"
 
 # MacPorts
 set -p PATH /opt/local/bin /opt/local/sbin
@@ -11,8 +20,3 @@ set -p PATH /opt/local/bin /opt/local/sbin
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-
-function fish_user_key_bindings
-    fzf_key_bindings
-end
-source /opt/local/share/fzf/shell/key-bindings.fish
