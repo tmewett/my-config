e77206a2e3c65d809286d5a53e485fc498480c46
Tom M <tom@tmewett.com>
2025-06-09 12:34:02 +0100

    sync

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,4 +1,8 @@
 function fish_title
-	echo $_ ' '
-	prompt_pwd
+	echo -n $_ ' '
+	set cwd (string split / -- (prompt_pwd))
+	echo -n $cwd[-1]
+	for part in $cwd[-2..1]
+		echo -n "\\$part"
+	end
 end
