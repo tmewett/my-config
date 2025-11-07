8b747edd2acd156192f1da19f78f20c59341bf97
Tom M <tom@tmewett.com>
2025-11-07 16:59:10 +0000

    hc_end and other stuff

--- home-configuration.fish
+++ home-configuration.fish
@@ -16,14 +16,24 @@ end
 set -q textadept; hc_if
     hc_then _dl_ta
     hc_then_symlink $HOME/.textadept-bin/textadept $HOME/.local/bin/textadept
-    # hc_then_symlink $my_config_dir/textadept/init.lua $HOME/.config/
+    hc_then_symlink $my_config_dir/textadept/init.lua $HOME/.config/
+hc_end
 
-set -q kakoune_dir; hc_if
+false; hc_if
     hc_then_symlink $kakoune_dir/src/kak $HOME/.local/bin/kak
     hc_then_symlink $kakoune_dir/rc $HOME/.config/kak/autoload/rc
     hc_then_symlink $my_config_dir/kakoune $HOME/.config/kak/autoload/my_config
+hc_end
 
 test -d /etc/apparmor.d; hc_if
     hc_then_sudo_put_file $my_config_dir/bwrap.apparmor /etc/apparmor.d/bwrap
+hc_end
 
+true; hc_if
+    hc_then_symlink $my_config_dir/settings.json ~/.config/Code/User/settings.json
+    hc_then_symlink $my_config_dir/keybindings.json ~/.config/Code/User/keybindings.json
+    hc_then_symlink $my_config_dir/snippets ~/.config/Code/User/snippets
+hc_end
+
+hc_safe_symlink $my_config_dir/fish/functions ~/.config/fish/functions
 hc_safe_rm_symlink ~/.config/fish/conf.d
