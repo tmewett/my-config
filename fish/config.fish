cb4b1b533d0d6a32d65419fb08a11a54f5ef8f47
Tom M <tom@tmewett.com>
2026-01-07 17:37:12 +0000

    less -F

--- fish/config.fish
+++ fish/config.fish
@@ -5,11 +5,12 @@ set -x MSYS2_ENV_CONV_EXCL "$MSYS2_ENV_CONV_EXCL;GEM_PATH"
 # reset to default in case it was set in a parent
 set -e MSYS2_ARG_CONV_EXCL
 
+# -F - quit if one screen
 # -j .5 - search results appear in middle of screen
 # -R - don't escape colours and text effects
 # -P prompt - the default medium/-m prompt, modified to always show the file
 #   (not just on first prompt) and appended with help info like 'man'
-set -x LESS "-j .5 -R -P ?f%f :- .?m(%T %i of %m) .?e(END) ?x- Next\: %x.:?pB%pB\%:byte %bB?s/%s...%t (press h for help or q to quit)\$"
+set -x LESS "-F -j .5 -R -P ?f%f :- .?m(%T %i of %m) .?e(END) ?x- Next\: %x.:?pB%pB\%:byte %bB?s/%s...%t (press h for help or q to quit)\$"
 
 # this doesn't take into account ignorefiles
 set -x FZF_DEFAULT_COMMAND "find . ! -type d -printf '%P\n'"
