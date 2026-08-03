242ca8a9d9727441292d41ed2e14f5488a1b846a
Tom M <tom@tmewett.com>
2026-08-03 10:43:52 +0000

    un-if phoenix backup stuff

--- fish/config.fish
+++ fish/config.fish
@@ -93,22 +93,20 @@ if status is-interactive
             x gnome-terminal -- $cmd
         end
     end
-    if test $(hostname) = "phoenix"
-        function prepare-backup
-            pushd $tom/files/projects
-            or return 1
-            for d in *
-                cd $d
-                w git gc
-                cd ..
-            end
-            popd
+    function prepare-backup
+        pushd $tom/files/projects
+        or return 1
+        for d in *
+            cd $d
+            w git gc
+            cd ..
         end
-        function backup
-            pushd $tom/files
-            or return 1
-            ~/borg.venv/bin/python -m borg create -C auto,zstd --progress --patterns-from borg-filter ::windows-from-msys2-\{now\} . ../AppData/vault
-        end
+        popd
+    end
+    function backup
+        pushd $tom/files
+        or return 1
+        ~/borg.venv/bin/python -m borg create -C auto,zstd --progress --patterns-from borg-filter ::windows-from-msys2-\{now\} . ../AppData/vault
     end
 end
 
